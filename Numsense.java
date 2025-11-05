d603b0445de12220208e5c7ed2c40e9569525a62
Leo-Perio <leogperio08@gmail.com>
2025-11-05 09:41:12 -0600

    Added main file
    Created the first file in the program

---
+++ Numsense.java
@@ -0,0 +1,9 @@
+public class Numsense
+{
+	public static void Main(String[] args)
+	{
+		System.out.println("Select the one you would like to practice: \n");
+		System.out.println("1. Addition");
+		System.out.println("
+	}
+}
